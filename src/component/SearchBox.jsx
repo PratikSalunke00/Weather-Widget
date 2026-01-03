a277fb4aa08e0626fcb1a1ab6d865e705c639dc5
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-01-03 19:13:21 +0530

    wwather api used and understand that how  api works

--- src/component/SearchBox.jsx
+++ src/component/SearchBox.jsx
@@ -2,9 +2,31 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import "./SearchBox.css";
 import { use, useState } from "react";
+import.meta.env;
 
 function SearchBox() {
   let [city, setCity] = useState("");
+
+  const API_URL = VITE_API_URL;
+  const API_KEY = VITE_API_KEY;
+
+  let getWeatherInfo = async () => {
+    let response = await fetch(
+      `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    let jsonResponse = await response.json();
+
+    let result = {
+      temperature: jsonResponse.main.temp,
+      temperatureMin: jsonResponse.main.temp_min,
+      temperatureMax: jsonResponse.main.temp_max,
+      humidity: jsonResponse.main.humidity,
+      feelslike: jsonResponse.main.feels_like,
+      windSpeed: jsonResponse.wind.speed,
+      description: jsonResponse.weather[0].description,
+    };
+    console.log(result);
+  };
 
   let handleChange = (event) => {
     setCity(event.target.value);
@@ -14,6 +36,7 @@ function SearchBox() {
     event.preventDefault();
     console.log("Searching for city:", city);
     setCity("");
+    getWeatherInfo();
   };
 
   return (
